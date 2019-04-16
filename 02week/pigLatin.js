b0cda32a12997c3362fcd97dd8aab2d3f2a9315f
Kiley Lewis <kileylewis32@gmail.com>
2019-04-15 19:21:03 -0500

    pig latin finished

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -13,6 +13,20 @@ function pigLatin(str) {
 // trimmed (no spaces before or after) and lowercase. .trim() .toLowercase()
   str = str.trim().toLowercase()
 // put first letter at end of word, add "ay".
+  const vowels = ["a", "e", "i", "o", "u"];
+  let vowelIndex = 0;
+
+  if (vowels.includes(str[o])) {
+    return str + "yay";
+  } else {
+    for (let char of str) {
+      if(vowels.includes(char)) {
+        vowelIndex = str.indexOf(char);
+        break;
+      }
+    }
+    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+  }
 
 
 }
