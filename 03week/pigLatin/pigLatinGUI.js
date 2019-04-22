d37e9a73eab0f22576e8f3a574cbbbaba7562f58
Kiley Lewis <kileylewis32@gmail.com>
2019-04-21 22:10:33 -0500

    update

--- 03week/pigLatin/pigLatinGUI.js
+++ 03week/pigLatin/pigLatinGUI.js
@@ -1,19 +1,64 @@
-// trimmed (no spaces before or after) and lowercase. .trim() .toLowercase()
-str = str.trim().toLowercase()
-// put first letter at end of word, add "ay".
-  const vowels = ["a", "e", "i", "o", "u"];
-  let vowelIndex = 0;
+'use strict';
 
-  if (vowels.includes(str[o])) {
-    return str + "yay";
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function pigLatin(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'],
+    trimmedWord = str.trim().toLowerCase(),
+    result = trimmedWord.split('');
+
+  if (vowels.includes(trimmedWord.charAt(0))) {
+    return str += 'yay';
   } else {
-    for (let char of str) {
-      if(vowels.includes(char)) {
-        vowelIndex = str.indexOf(char);
-        break;
+    for (var i = 0; i < trimmedWord.length; i++) {
+      if (!vowels.includes(trimmedWord[i])) {
+        result.push(result.shift());
+      } else {
+        result.push('ay');
+        return result.join('');
       }
     }
-    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
   }
+}
 
 
+function getPrompt() {
+  rl.question('word ', (answer) => {
+    console.log( pigLatin(answer) );
+    getPrompt();
+  });
+}
+
+// Tests
+
+if (typeof describe === 'function') {
+
+  describe('#pigLatin()', () => {
+    it('should translate a simple word', () => {
+      assert.equal(pigLatin('car'), 'arcay');
+      assert.equal(pigLatin('dog'), 'ogday');
+    });
+    it('should translate a complex word', () => {
+      assert.equal(pigLatin('create'), 'eatecray');
+      assert.equal(pigLatin('valley'), 'alleyvay');
+    });
+    it('should attach "yay" if word begins with vowel', () => {
+      assert.equal(pigLatin('egg'), 'eggyay');
+      assert.equal(pigLatin('emission'), 'emissionyay');
+    });
+    it('should lowercase and trim word before translation', () => {
+      assert.equal(pigLatin('HeLlO '), 'ellohay');
+      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+    });
+  });
+} else {
+
+  getPrompt();
+
+}
